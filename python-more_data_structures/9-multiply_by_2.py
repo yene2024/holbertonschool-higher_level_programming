f38fa007aa6b3064b93afe1fc8debf488a132136
yene2024 <yeneishla1524@gmail.com>
2024-01-29 22:04:54 -0400

    fixing a function that returns a new dictionary with all values multiplied by 2

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,3 @@
 #!usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_directory = {}
-    for key, value in a_dictionary.items():
-        new_directory = value * 2
-    return new_directory
+    return {key: value * 2 for key, value in a_dictionary.items()}
