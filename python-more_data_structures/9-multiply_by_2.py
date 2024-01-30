2de606ca19dd61d342c806d2eecf87d5d74e3d96
yene2024 <yeneishla1524@gmail.com>
2024-01-29 22:25:11 -0400

    fixing function.

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!usr/bin/python3
 def multiply_by_2(a_dictionary):
-    multiplied_dict = {}
-    for key, value in a_dictionary.items():
-        multiplied_dict[key] = value * 2
-    return multiplied_dict
+    new_dictionary = dict(a_dictionary)
+    for key in new_dictionary:
+        new_dictionary *= 2
+    return new_dictionary
