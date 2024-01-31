e24398e13b935a145465a1416dbae0aa8641422d
yene2024 <yeneishla1524@gmail.com>
2024-01-31 15:46:40 -0400

    Writing a class Square that defines a square

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""""
+Square with size
+A class Sqaure that defines a sqaure
+"""
+
+
+class Square:
+    """Class Square that defines a square"""
+    def __init__(self, size):
+        """Define Private instance attribute: size"""
+        self.__size = size
