d5f9bc4bc328c635125c3cdb735ccc935f1e3760
yene2024 <yeneishla1524@gmail.com>
2024-02-07 11:31:37 -0400

    Detect instance deletion

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+
+"""5. Detect instance deletion"""
+
+
+class Rectangle:
+    """Class that defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Method that initializes the instance"""
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Method that returns the value of the width"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Function that sets width instance attribute
+        Raise a TypeError & ValueError if not integer of natural number resp.
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """Method that returns the value of the height"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Function that sets height instance attribute
+        Raise a TypeError & ValueError if not integer of natural number resp.
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    def area(self):
+        """Method that returns the rectangle area"""
+
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """Method that returns the rectangle perimeter"""
+
+        if self.__width == 0 or self.__height == 0:
+            return 0
+
+        return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        """Method that returns the string representation of the rectangle"""
+
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        return (("#" * self.__width + "\n") * self.__height)[:-1]
+
+    def __repr__(self):
+        """Method that returns the representation of the rectangle"""
+
+        return ("Rectangle({}, {})".format(self.__width, self.__height))
+
+    def __del__(self):
+        """Method that deletes the instance"""
+
+        print("Bye rectangle...")
