5d6fbed8b9cbc0bd2261e2be2e92d80407f83232
yene2024 <yeneishla1524@gmail.com>
2024-02-14 12:02:33 -0400

    Fixing the code

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,60 +1,65 @@
 #!/usr/bin/python3
-""" Square class """
+
+"""Square Model"""
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Square class inherits from Rectangle class"""
-    # constructor
+    """A class that defines a Square model"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """ init method for square class
-        Args:
-            size (int): size of the square
-            x (int): x position of the square
-            y (int): y position of the square
-            id (int): id of the square
-        """
+        """A function that initializes square instances"""
+
         super().__init__(size, size, x, y, id)
 
-    def __str__(self):
-        """ string representation of square """
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
-
-    # Getters
     @property
     def size(self):
-        """ size getter """
-        return self.width
+        """A function that defines size attribute of a square model"""
 
-    # Setters
+        return (self.width)
+
     @size.setter
     def size(self, value):
-        """ size setter
-        Args:
-            value (int): value to set size to"""
+        """A function that passes size attribute of a square model"""
+
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
-        """update method for square class
-        Args:
-            args (list): list of arguments
-            kwargs (dict): dictionary of arguments
-        """
-        if args:
-            if len(args) >= 1:
-                self.id = args[0]
-            if len(args) >= 2:
-                self.size = args[1]
-            if len(args) >= 3:
-                self.x = args[2]
-            if len(args) >= 4:
-                self.y = args[3]
-        else:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+        """A function that assigns key/value argument to square attributes"""
+
+        if len(args) == 0:
+            for key, val in kwargs.items():
+                self.__setattr__(key, val)
+
+            return
+
+        try:
+
+            self.id = args[0]
+            self.size = args[1]
+            self.x = args[2]
+            self.y = args[3]
+
+        except IndexError:
+            pass
+
+    def __str__(self):
+        """A function that returns string format of a square representation"""
+
+        return ("[{}] ({}) {}/{} - {}".format(type(self).__name__,
+                self.id, self.x, self.y, self.width))
 
     def to_dictionary(self):
-        """ dictionary representation of square """
-        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
+        """A function that returns dictionary representation of a Square"""
+
+        return ({'id': getattr(self, "id"),
+                'size': getattr(self, "width"),
+                 'x': getattr(self, "x"), 'y': getattr(self, "y")})
