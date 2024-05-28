7f53ae94f19ab40c7893c636810f6ed2abefc033
yene2024 <yeneishla1524@gmail.com>
2024-05-28 11:20:39 -0400

    Writing a JavaScript script that updates the text of the header element to New Header!!! when the user clicks on the element with id update_header

--- javascript-dom_manipulation/5-script.js
+++ javascript-dom_manipulation/5-script.js
@@ -1,7 +1,3 @@
-var update_header = document.getElementById('update_header')
-
-update_header.addEventListener('click', function(){
-    var header = document.querySelector('header');
-
-    header.textContent = "New Header!!!"
-})
+$('DIV#add_item').click(() => {
+    $('UL.my_list').append('<li>Item</li>');
+});
