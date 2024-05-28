cfc65625a29d22b8be188b8090bc2e0824b6e1c3
yene2024 <yeneishla1524@gmail.com>
2024-05-28 11:19:38 -0400

    Writing a JavaScript script that adds the class red to the header element when the user clicks on the tag with id red_header

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,6 +1,3 @@
-var red_header = document.getElementById('red_header')
-
-red_header.addEventListener('click', function(){
-    var header = document.querySelector('header');
-    header.classList.add("red");
-})
+$('DIV#red_header').click(function () {
+    $('HEADER').addClass('red');
+  });
